b49e0cbf22127bbf50330de0d34e66c68e109450
Dónal McManus <donal.mcm@gmail.com>
2019-02-19 22:30:24 +0000

    array of versions added and loop to print out each version

--- removePlatformTool.sh
+++ removePlatformTool.sh
@@ -4,3 +4,12 @@
 cd C:/Users/Visitor/Desktop
 
 echo "*** Please enter a platform you wish to remove ***"
+
+# List of current platforms - change the names to your platforms 
+platforms=(version1 version2 version3 version4)
+echo ""
+for i in "${platforms[@]"; do
+ echo "$i"
+ done
+echo ""
+echo "**************************************************"
