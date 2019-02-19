d90b6014617ee9017d3af1ed8b4f77ce3850259b
Dónal McManus <donal.mcm@gmail.com>
2019-02-19 22:26:51 +0000

    location where the script will run and first message to user added

--- removePlatformTool.sh
+++ removePlatformTool.sh
@@ -1,1 +1,6 @@
 # A tool to remove a specified platform or version
+
+# Change location to where your platorms are stored
+cd C:/Users/Visitor/Desktop
+
+echo "*** Please enter a platform you wish to remove ***"
