efa914999df92452b88884a62766fe9ed4e40c83
Dónal McManus <donal.mcm@gmail.com>
2019-02-19 23:21:28 +0000

    find and list all identical files with hash of file

--- mergeTool.sh
+++ mergeTool.sh
@@ -1,1 +1,11 @@
 # A tool to merge identical files
+
+# Change to where your identical files are
+cd C:/Users/Visitor/Desktop
+
+# Finds and lists all duplicate files
+find . -type f -exec md5sum {} + | sort | uniq -w32 -dD
+
+# keep one version of identical files and delete the rest
+
+# rename remaining file with common name
